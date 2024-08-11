eb215437182a1e101426f1725150a5f28e154635
fvdime <73942727+fvdime@users.noreply.github.com>
2024-08-11 21:28:45 +0300

    feat: add clipboard component

---
+++ lib/Clipboard/clipboard.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Clipboard } from "./clipboard";
+
+const meta: Meta<typeof Clipboard> = {
+  component: Clipboard,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Clipboard>;
+
+export const Default: Story = {
+  args: {
+    content: "npm install merelyui",
+    children: (
+      <h5 className="font-medium"><span className="text-amber-300 me-1">npm</span>install merelyui</h5>
+    ),
+    terminal: true,
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,33 @@ export default {
           },
         },
       },
+      zoomIn: {
+          "0%": {
+            opacity: 0,
+            transform: "scale3d(0.3, 0.3, 0.3)",
+          },
+          "80%": {
+            opacity: 0.8,          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+        zoomOut: {
+          "0%": {
+            opacity: 1,
+          },
+          "15%": {
+            opacity: 0.8,
+          },
+          "100%": {
+            opacity: 0,
+            transform: "scale3d(0.3, 0.3, 0.3)",
+          },
+        },
       animation: {
         slideIn: 'slideIn 0.5s ease-in-out',
+        zoomIn: "zoom-in 1s ease-out 0.25s 1",
+        zoomOut: "zoom-out 1s ease-out 0.25s 1",
       },
     },
   },
