fba745e376de127906ac0fe91107b563ac271323
fvdime <73942727+fvdime@users.noreply.github.com>
2024-08-16 21:22:31 +0300

    fix: fix button file name

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,6 @@
 import "./merelyui.css"
 
-export { Button } from "./Button/Button";
+export { Button } from "./Button/button";
 export { IconButton } from "./IconButton/icon-button";
 export { Input } from "./Input/input";
 export { IconInput } from "./IconInput/icon-input";
