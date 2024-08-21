432f01245d3fa2aade0f368e885fcbb51d8c3e66
fvdime <73942727+fvdime@users.noreply.github.com>
2024-08-22 00:46:55 +0300

    fix import

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,6 @@
 import "./merelyui.css"
 
-export { Button } from "./Button/button";
+export { Button } from "./Button/Button";
 export { IconButton } from "./IconButton/icon-button";
 export { Input } from "./Input/input";
 export { IconInput } from "./IconInput/icon-input";
